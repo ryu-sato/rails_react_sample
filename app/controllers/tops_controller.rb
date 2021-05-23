26a81914b70f787a007b47652bda7a2f650d44ff
Ryu Sato <ryu@weseek.co.jp>
2021-05-24 00:54:36 +0900

    initialize react

---
+++ app/controllers/tops_controller.rb
@@ -0,0 +1,5 @@
+class TopsController < ApplicationController
+  # GET /tops or /tops.json
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root 'tops#index'
+  resources :tops
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
