3c659cdaafd80955f8b98b4416216a3ff707bc11
Ryu Sato <ryu@weseek.co.jp>
2021-06-10 02:45:44 +0900

    kuromojiを導入

--- app/models/concerns/full_text_searchable.rb
+++ app/models/concerns/full_text_searchable.rb
@@ -9,6 +9,36 @@
 module FullTextSearchable
   extend ActiveSupport::Concern
   
+  ES_SETTINGS = {
+    analysis: {
+      analyzer: {
+        kuromoji: {
+          tokenizer: :kuromoji_tokenizer,
+          char_filter: %i[icu_normalizer]
+        },
+        ngram: {
+          tokenizer: :ngram,
+          char_filter: %i[icu_normalizer]
+        }
+      }
+    }
+  }
+
+  ANALYZER_SETTINGS = {
+    raw: {
+      type: :text,
+      analyzer: :keyword
+    },
+    ngram: {
+      type: :text,
+      analyzer: :ngram
+    },
+    kuromoji: {
+      type: :text,
+      analyzer: :kuromoji
+    }
+  }
+
   included do
     include Elasticsearch::Model
 
@@ -18,15 +48,16 @@ module FullTextSearchable
       all_attrs = self.attribute_names.map(&:to_sym)
       raise "All of #{indexed_attrs} must be includeded in #{all_attrs}" unless indexed_attrs.all?{|a| all_attrs.include?(a) }
 
+      mapping_attrs(indexed_attrs) do |attr|
+        mappings do
+          indexes attr, fields: ANALYZER_SETTINGS
+        end
+      end
+
       no_indexed_attrs = all_attrs - indexed_attrs
-      no_indexed_attrs.each do |attr|
-        begin
-          mappings do
-            indexes attr, enabled: false, type: "object"
-          end
-        rescue => exception
-          # workaround: https://github.com/activeadmin/activeadmin/issues/783
-          true
+      mapping_attrs(no_indexed_attrs) do |attr|
+        mappings do
+          indexes attr, enabled: false, type: "object"
         end
       end
     end
@@ -42,6 +73,23 @@ module FullTextSearchable
         __elasticsearch__.delete_document if ElasticsearchService.es_active?
       rescue Elasticsearch::Transport::Transport::Errors::NotFound
         # ドキュメントが存在しない(すでに削除済みの)時には例外を出力しないようにする
+      end
+    end
+    
+    private_class_method
+
+    def self.mapping_attrs(attrs, &mapping_block)
+      raise 'blcok is required' unless mapping_block
+
+      attrs.each do |attr|
+        settings ES_SETTINGS do
+          begin
+            mapping_block.call(attr)
+          rescue => exception
+            # workaround: https://github.com/activeadmin/activeadmin/issues/783
+            true
+          end
+        end
       end
     end
   end
